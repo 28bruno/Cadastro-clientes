be4e695ca7e20a9e7001984cd7514c716c385f60
28bruno <bruno.developer2019@gmail.com>
2023-11-13 10:55:20 -0300

    Criação do sistema de cadastros de clientes

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Clientes;
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function home()
+    {
+        $clientes = (Clientes::get());
+
+        return view('home', [
+            'clientes' => $clientes
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        $clientes = Clientes::find($id);
+
+        return view('show', [
+            'cliente' => $clientes
+        ]);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+}
---
+++ resources/views/show.blade.php
@@ -0,0 +1,22 @@
+@extends('app')
+
+@section('titulo', 'Cliente')
+
+@section('conteudo')
+    <div class="card">
+        <div class="card-header">
+            Detalhes do cliente
+        </div>
+        <div class="card-body">
+            <strong>Cód Bom Controle: </strong> {{ $cliente->codBomControle }}<br>
+            <strong>Razão Social: </strong> {{ $cliente->razaoSocial }}<br>
+            <strong>Nome Fantasia: </strong> {{ $cliente->nomeFantasia }}<br>
+            <strong>CNPJ: </strong> {{ $cliente->cnpj }}<br>
+            <strong>CPF: </strong> {{ $cliente->cpf }}<br>
+            <strong>Criado em: </strong> {{ $cliente->created_at }}<br>
+            <strong>Modificado em: </strong> {{ $cliente->updated_at }}<br>
+            <br>
+            <a class="btn btn-success" href="{{ route('home') }}">Voltar</a>
+        </div>
+    </div>
+@endsection
