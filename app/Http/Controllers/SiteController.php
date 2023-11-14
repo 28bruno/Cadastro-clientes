cc2e804fec560d304e65bd9d8e1aa11e1d0991ce
28bruno <bruno.developer2019@gmail.com>
2023-11-14 16:07:54 -0300

    Resolvido conflito de merge em app/Http/Controllers/SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -5,32 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Clientes;
 use Illuminate\Http\Request;
 
-class SiteController extends Controller
+class SiteController extends Controller 
 {
-    public function home()
+    public function home() 
     {
-        $clientes = (Clientes::get());
+        $clientes = ( Clientes::get() );
 
-        return view('home', [
+        return view( 'home', [
             'clientes' => $clientes
-        ]);
+        ] );
     }
 
-    public function show(int $id)
+    public function show( int $id ) 
     {
-        $clientes = Clientes::find($id);
+        $clientes = Clientes::find( $id );
 
-        return view('show', [
+        return view( 'show', [
             'cliente' => $clientes
-        ]);
+        ] );
     }
-<<<<<<< HEAD
-    
-=======
-
-    public function create()
-    {
-        return view('create');
-    }
->>>>>>> be4e695ca7e20a9e7001984cd7514c716c385f60
 }
