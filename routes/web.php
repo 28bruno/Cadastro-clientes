781bb653e6ae890edcd688c9c13956a533e14ec0
28bruno <bruno.developer2019@gmail.com>
2023-11-14 16:04:17 -0300

    Resolvido conflito de merge em routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\ClienteController;
-=======
->>>>>>> be4e695ca7e20a9e7001984cd7514c716c385f60
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +11,6 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-<<<<<<< HEAD
 | be assigned to the 'web' middleware group. Make something great!
 |
 */
@@ -23,13 +19,4 @@ Route::get( '/', [ SiteController::class, 'home' ] )->name( 'home' );
 Route::get( '/{id}', [ SiteController::class, 'show' ] )->name( 'clientes.show' );
 Route::get( '/clientes/create', [ ClienteController::class, 'create' ] )->name( 'clientes.create' );
 
-Route::post( '/clientes', [ClienteController::class, 'store'])->name('clientes.store');
-=======
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', [SiteController::class, 'home'])->name('home');
-Route::get('/create', [SiteController::class, 'create'])->name('clientes.create');
-Route::get('/{id}', [SiteController::class, 'show'])->name('clientes.show');
->>>>>>> be4e695ca7e20a9e7001984cd7514c716c385f60
+Route::post( '/clientes', [ ClienteController::class, 'store' ] )->name( 'clientes.store' );
